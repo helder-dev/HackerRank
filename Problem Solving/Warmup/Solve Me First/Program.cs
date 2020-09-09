54a8a8760eb0a8f30f4735fc72209552c5217675
helder-dev <helder.oliveira.dev@gmail.com>
2020-09-09 18:12:02 +0100

    Added solution to 'Solve Me First' problem

---
+++ Problem Solving/Warmup/Solve Me First/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Solve_Me_First {
+    class Program {
+        private static int SolveMeFirst (int a, int b) {
+            return a + b;
+        }
+
+        static void Main (string[] args) {
+            int val1 = Convert.ToInt32 (Console.ReadLine ());
+            int val2 = Convert.ToInt32 (Console.ReadLine ());
+
+            int sum = SolveMeFirst (val1, val2);
+
+            Console.WriteLine (sum);
+        }
+    }
+}
