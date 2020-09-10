a043e2711206bd1fbe49949a0c775c97c5374153
helder-dev <helder.oliveira.dev@gmail.com>
2020-09-10 09:44:39 +0100

    Added solution to 'Simple Array Sum' problem

---
+++ Problem Solving/Warmup/Simple Array Sum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Simple_Array_Sum {
+    class Program {
+        private static int SimpleArraySum(int[] ar) {
+            return ar.Sum();
+        }
+
+        static void Main(string[] args) {
+            int arCount = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+
+            int result = SimpleArraySum(ar);
+
+            Console.WriteLine(result);
+        }
+    }
+}
