9955a2a98f534c95f1b6cfbbdff59e90fd2089af
helder-dev <helder.oliveira.dev@gmail.com>
2020-09-10 11:16:08 +0100

    Added solution to 'Birthday Cake Candles' problem

---
+++ Problem Solving/Warmup/Birthday Cake Candles/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Birthday_Cake_Candles {
+    class Program {
+        private static int BirthdayCakeCandles(int[] ar) {
+            int max = ar.Max();
+
+            return ar.Count(x => x == max);
+        }
+
+        static void Main(string[] args) {
+            int arCount = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+
+            int result = BirthdayCakeCandles(ar);
+
+            Console.WriteLine(result);
+        }
+    }
+}
