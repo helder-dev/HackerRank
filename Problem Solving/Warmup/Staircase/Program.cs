b40d7db5edbc75b3341d8d5a76cc5ff9f3f3dac7
helder-dev <helder.oliveira.dev@gmail.com>
2020-09-10 11:04:04 +0100

    Added solution to 'Staircase' problem

---
+++ Problem Solving/Warmup/Staircase/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Staircase {
+    class Program {
+        private static void Staircase(int n) {
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine(new String(' ', n - i) + new String('#', i));
+            }
+        }
+
+        static void Main(string[] args) {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Staircase(n);
+        }
+    }
+}
