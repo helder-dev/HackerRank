966a43dafdfa9017e32319a871331f07fc347a63
helder-dev <helder.oliveira.dev@gmail.com>
2020-09-10 16:21:11 +0100

    Added solution to 'Grading Students' problem

---
+++ Problem Solving/Implementation/Grading Students/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Grading_Students {
+    class Program {
+        public static int GradingStudent(int grade) {
+            if (grade < 38) {
+                return grade;
+            } else {
+                int mod = (grade % 5);
+                if (mod == 0) {
+                    return grade;
+                } else if ((5 - mod) < 3) {
+                    return grade + (5 - mod);
+                } else {
+                    return grade;
+                }
+            }
+        }
+
+        static void Main(string[] args) {
+            int gradesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int i = 0; i < gradesCount; i++) {
+                int gradesItem = Convert.ToInt32(Console.ReadLine().Trim());
+
+                Console.WriteLine(GradingStudent(gradesItem));
+            }
+        }
+    }
+}
