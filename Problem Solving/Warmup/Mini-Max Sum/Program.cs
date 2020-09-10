ac9d9db42bfbbd30736a5cbee91830e9e0051342
helder-dev <helder.oliveira.dev@gmail.com>
2020-09-10 11:13:12 +0100

    Added solution to 'Mini-Max Sum' problem

---
+++ Problem Solving/Warmup/Mini-Max Sum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Mini_Max_Sum {
+    class Program {
+        private static void MiniMaxSum(int[] ar) {
+            Array.Sort(ar);
+
+            Console.WriteLine(
+                String.Format("{0} {1}",
+                    ar.Reverse().Skip(1).Sum(x => Convert.ToInt64(x)),
+                    ar.Skip(1).Sum(x => Convert.ToInt64(x))
+                )
+            );
+        }
+
+        static void Main(string[] args) {
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+            MiniMaxSum(arr);
+        }
+    }
+}
