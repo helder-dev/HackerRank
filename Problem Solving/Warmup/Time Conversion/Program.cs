3bed7e066cc5cbc7d539a7ab9cc767e7b35fc480
helder-dev <helder.oliveira.dev@gmail.com>
2020-09-10 11:19:11 +0100

    Added solution to 'Time Conversion' problem

---
+++ Problem Solving/Warmup/Time Conversion/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Time_Conversion {
+    class Program {
+        private static string TimeConversion(string s) {
+            return Convert.ToDateTime(s).ToString("HH:mm:ss");
+        }
+
+        static void Main(string[] args) {
+            string s = Console.ReadLine();
+
+            string result = TimeConversion(s);
+
+            Console.WriteLine(result);
+        }
+    }
+}
