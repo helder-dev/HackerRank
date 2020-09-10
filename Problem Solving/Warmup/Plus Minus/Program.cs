54b05a5a6a4390bd0e65ab3c4c277c33d2a1c666
helder-dev <helder.oliveira.dev@gmail.com>
2020-09-10 10:59:46 +0100

    Added solution to 'Plus Minus' problem

---
+++ Problem Solving/Warmup/Plus Minus/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Plus_Minus {
+    class Program {
+        private static void PlusMinus(int arCount, int[] ar) {
+            Console.WriteLine(Math.Round(ar.Count(x => x > 0) / (decimal)arCount, 6));
+            Console.WriteLine(Math.Round(ar.Count(x => x < 0) / (decimal)arCount, 6));
+            Console.WriteLine(Math.Round(ar.Count(x => x == 0) / (decimal)arCount, 6));
+        }
+
+        static void Main(string[] args) {
+            int arCount = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+
+            PlusMinus(arCount, ar);
+        }
+    }
+}
