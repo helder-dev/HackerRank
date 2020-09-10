646b61ad2bde4a86f7efc66846c9b18ce0a17783
helder-dev <helder.oliveira.dev@gmail.com>
2020-09-10 09:51:53 +0100

    Added solution to 'Compare the Triplets' problem

---
+++ Problem Solving/Warmup/Compare the Triplets/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Compare_the_Triplets {
+    class Program {
+        private static int[] CompareTriplets(int[] arA, int[] arB) {
+            int scoreA = 0,
+                scoreB = 0;
+
+            for (int i = 0; i < arA.Length; i++) {
+                if (arA[i] > arB[i]) {
+                    scoreA++;
+                } else if (arA[i] < arB[i]) {
+                    scoreB++;
+                }
+            }
+
+            return new int[] { scoreA, scoreB };
+        }
+
+        static void Main(string[] args) {
+            int[] arA = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+            int[] arB = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+
+            int[] score = CompareTriplets(arA, arB);
+
+            Console.WriteLine(String.Join(" ", score));
+        }
+    }
+}
