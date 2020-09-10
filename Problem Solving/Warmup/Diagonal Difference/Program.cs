270a1046f0634233e601bf97ab4e97b962e4e1c4
helder-dev <helder.oliveira.dev@gmail.com>
2020-09-10 10:48:35 +0100

    Added solution to 'Diagonal Difference' problem

---
+++ Problem Solving/Warmup/Diagonal Difference/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Diagonal_Difference {
+    class Program {
+        public static int DiagonalDifference(int[][] ar) {
+            int firstDiagonal = 0;
+            int secondDiagonal = 0;
+
+            for (int i = 0; i < ar.Length; i++) {
+                firstDiagonal = firstDiagonal + ar[i][i];
+                secondDiagonal = secondDiagonal + ar[i][(ar.Length - 1) - i];
+            }
+
+            return Math.Abs(firstDiagonal - secondDiagonal);
+        }
+
+        static void Main(string[] args) {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[][] ar = new int[n][];
+
+            for (int i = 0; i < n; i++) {
+                ar[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+            }
+
+            int result = DiagonalDifference(ar);
+
+            Console.WriteLine(result);
+        }
+    }
+}
