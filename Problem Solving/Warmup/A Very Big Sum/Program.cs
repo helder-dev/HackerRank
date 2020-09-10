49753b8e1d82972ad6e223e21d2df8bc742f1477
helder-dev <helder.oliveira.dev@gmail.com>
2020-09-10 10:27:38 +0100

    Added solution to 'A Very Big Sum' problem

---
+++ Problem Solving/Warmup/A Very Big Sum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace A_Very_Big_Sum {
+    class Program {
+        private static long VeryBigSum(long[] ar) {
+            return ar.Sum();
+        }
+
+        static void Main(string[] args) {
+            int arCount = Convert.ToInt32(Console.ReadLine());
+
+            long[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt64(arTemp));
+
+            long result = VeryBigSum(ar);
+
+            Console.WriteLine(result);
+        }
+    }
+}
