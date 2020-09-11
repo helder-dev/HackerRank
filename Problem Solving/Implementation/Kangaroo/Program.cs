32f0b075c8f55b56f4aa52beef5a07f4d9e622ef
helder-dev <helder.oliveira.dev@gmail.com>
2020-09-11 11:11:24 +0100

    Added solution to 'Kangaroo' problem

---
+++ Problem Solving/Implementation/Kangaroo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Kangaroo {
+    class Program {
+        public static void Kangaroo(int x1, int v1, int x2, int v2) {
+            if ((v1 > v2) && ((x2 - x1) % (v1 - v2) == 0)) {
+                Console.WriteLine("YES");
+            } else {
+                Console.WriteLine("NO");
+            }
+        }
+
+        static void Main(string[] args) {
+            string[] values = Console.ReadLine().Split(' ');
+
+            int x1 = Convert.ToInt32(values[0]);
+            int v1 = Convert.ToInt32(values[1]);
+            int x2 = Convert.ToInt32(values[2]);
+            int v2 = Convert.ToInt32(values[3]);
+
+            Kangaroo(x1, v1, x2, v2);
+        }
+    }
+}
