d9908d7344616fe7cefb0440b04a905a72b04112
helder-dev <helder.oliveira.dev@gmail.com>
2020-09-15 16:50:09 +0100

    Added solution to 'Breaking the Records' problem

---
+++ Problem Solving/Implementation/Breaking the Records/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Breaking_the_Records {
+    class Program {
+        public static int[] BreakingRecords(int[] scores) {
+            int highestScore = scores.First(),
+                lowestScore = scores.First(),
+                increased = 0,
+                decreased = 0;
+
+            for (int i = 0; i < scores.Length; i++) {
+                if (scores[i] > highestScore) {
+                    highestScore = scores[i];
+                    increased++;
+                } else if (scores[i] < lowestScore) {
+                    lowestScore = scores[i];
+                    decreased++;
+                }
+            }
+
+            return new int[] { increased, decreased };;
+        }
+
+        static void Main(string[] args) {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] scores = Array.ConvertAll(Console.ReadLine().Split(' '), scoresTemp => Convert.ToInt32(scoresTemp));
+
+            int[] result = BreakingRecords(scores);
+
+            Console.WriteLine("{0}", string.Join(" ", result));
+        }
+    }
+}
