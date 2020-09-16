f101d630e4582c3abe1fcdef9e187062e97c527b
helder-dev <helder.oliveira.dev@gmail.com>
2020-09-16 09:45:04 +0100

    Added solution to 'Birthday Chocolate' problem

---
+++ Problem Solving/Implementation/Birthday Chocolate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Birthday_Chocolate {
+    class Program {
+        public static int Birthday(int[] ar, int d, int m) {
+            int sum = 0,
+                count = 0;
+
+            for (int i = 0; i < ar.Length; i++) {
+                sum += ar[i];
+
+                if (i >= m)sum -= ar[i - m];
+                if (sum == d && i >= m - 1)count++;;
+            }
+
+            return count;
+        }
+
+        static void Main(string[] args) {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+
+            string[] dm = Console.ReadLine().TrimEnd().Split(' ');
+
+            int d = Convert.ToInt32(dm[0]);
+            int m = Convert.ToInt32(dm[1]);
+
+            int result = Birthday(ar, d, m);
+
+            Console.WriteLine(result);
+        }
+    }
+}
