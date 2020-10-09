a65b9028937c0b85b1493c14f6d8d146249593b5
helder-dev <helder.oliveira.dev@gmail.com>
2020-10-09 15:35:07 +0100

    Added solution to 'Sales by Match' problem

---
+++ Problem Solving/Implementation/Sales by Match/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Sales_by_Match {
+    class Program {
+        public static int SockMerchant(int[] ar) {
+            return ar.GroupBy(x => x).Select(group => group.Count() / 2).Sum();
+        }
+
+        static void Main(string[] args) {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+
+            int result = SockMerchant(ar);
+
+            Console.WriteLine(result);
+        }
+    }
+}
