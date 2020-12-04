54c8a7269ec34f917359338bde29ac5a682ec96c
helder-dev <helder.oliveira.dev@gmail.com>
2020-12-04 14:50:56 +0000

    Added solution to 'Counting Valleys' problem

---
+++ Problem Solving/Implementation/Counting Valleys/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Counting_Valleys {
+    class Program {
+        public static int CountingValleys(int n, string s) {
+            int level = 0;
+            int valleys = 0;
+
+            for (int i = 0; i < n; i++) {
+                if (s[i] == 'U') {
+                    if (++level == 0) {
+                        valleys++;
+                    }
+                } else {
+                    level--;
+                }
+            }
+
+            return valleys;
+        }
+
+        static void Main(string[] args) {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            string s = Console.ReadLine();
+
+            int result = CountingValleys(n, s);
+
+            Console.WriteLine(result);
+        }
+    }
+}
