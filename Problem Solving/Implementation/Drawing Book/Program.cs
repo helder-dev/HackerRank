a978fc251b0c1ca14f1fabd697fb7865484b9542
helder-dev <helder.oliveira.dev@gmail.com>
2020-12-04 14:46:52 +0000

    Added solution to 'Drawing Book' problem

---
+++ Problem Solving/Implementation/Drawing Book/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Drawing_Book {
+    class Program {
+        static int PageCount(int n, int p) {
+            return Math.Min(p / 2, n / 2 - p / 2);
+        }
+
+        static void Main(string[] args) {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int p = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("{0}", PageCount(n, p));
+        }
+    }
+}
