f0f72491b46db27aa9432976103e707375b7ae70
helder-dev <helder.oliveira.dev@gmail.com>
2021-02-24 16:45:58 +0000

    Added solution to 'Angry Professor' problem

---
+++ Problem Solving/Implementation/Angry Professor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Angry_Professor {
+    class Program {
+        public static string AngryProfessor(int k, int[] ar) {
+            return ar.Count(x => x <= 0) < k ? "YES" : "NO";
+        }
+
+        static void Main(string[] args) {
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < t; i++) {
+                string[] nk = Console.ReadLine().Split(' ');
+
+                int n = Convert.ToInt32(nk[0]);
+                int k = Convert.ToInt32(nk[1]);
+
+                int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+
+                string result = AngryProfessor(k, ar);
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
