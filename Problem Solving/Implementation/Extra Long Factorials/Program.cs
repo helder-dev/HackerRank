bcd6d2921bca0d07d18a1d1fd8a57d77bd136a2c
helder-dev <helder.oliveira.dev@gmail.com>
2021-02-24 18:18:27 +0000

    Added solution to 'Extra Long Factorials' problem

---
+++ Problem Solving/Implementation/Extra Long Factorials/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+namespace Extra_Long_Factorials {
+    class Program {
+        public static void ExtraLongFactorials(int n) {
+            BigInteger fact = n;
+
+            for (int i = (n - 1); i >= 1; i--) {
+                fact = fact * i;
+            }
+
+            Console.WriteLine(fact);
+        }
+
+        static void Main(string[] args) {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            ExtraLongFactorials(n);
+        }
+    }
+}
