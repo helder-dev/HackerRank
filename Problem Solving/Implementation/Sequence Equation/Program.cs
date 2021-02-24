498f01baf86546dd43823509054c31f24a348699
helder-dev <helder.oliveira.dev@gmail.com>
2021-02-24 18:07:31 +0000

    Added solution to 'Sequence Equation' problem

---
+++ Problem Solving/Implementation/Sequence Equation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Sequence_Equation {
+    class Program {
+        private static void PermutationEquation(int[] ar) {
+            for (int i = 1; i <= ar.Length; i++) {
+                Console.WriteLine(Array.IndexOf(ar, Array.IndexOf(ar, i) + 1) + 1);
+            }
+        }
+
+        static void Main(string[] args) {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), pTemp => Convert.ToInt32(pTemp));
+
+            PermutationEquation(ar);
+        }
+    }
+}
