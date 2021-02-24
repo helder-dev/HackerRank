09613f8394fec9a2c23f58660f78baa3975a65ca
helder-dev <helder.oliveira.dev@gmail.com>
2021-02-24 21:10:19 +0000

    Added solution to 'Sherlock and Squares' problem

---
+++ Problem Solving/Implementation/Sherlock and Squares/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Sherlock_and_Squares {
+    class Program {
+        private static int Squares(int a, int b) {
+            return Convert.ToInt32(Math.Floor(Math.Sqrt(b)) - Math.Floor(Math.Sqrt(a - 1)));
+        }
+
+        static void Main(string[] args) {
+            int q = Convert.ToInt32(Console.ReadLine());
+
+            for (int qItr = 0; qItr < q; qItr++) {
+                string[] ab = Console.ReadLine().Split(' ');
+
+                int a = Convert.ToInt32(ab[0]);
+                int b = Convert.ToInt32(ab[1]);
+
+                int result = Squares(a, b);
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
