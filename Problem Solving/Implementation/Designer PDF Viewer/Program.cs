03939795d9900b01f9f7264a0712129dae90f42a
helder-dev <helder.oliveira.dev@gmail.com>
2021-02-24 16:18:12 +0000

    Added solution to 'Designer PDF Viewer' problem

---
+++ Problem Solving/Implementation/Designer PDF Viewer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Designer_PDF_Viewer {
+    class Program {
+        public static int DesignerPdfViewer(int[] ar, string word) {
+            return word.Max(x => ar[x - 'a']) * word.Length;;
+        }
+
+        static void Main(string[] args) {
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+
+            string word = Console.ReadLine();
+
+            int result = DesignerPdfViewer(ar, word);
+
+            Console.WriteLine(result);
+        }
+    }
+}
