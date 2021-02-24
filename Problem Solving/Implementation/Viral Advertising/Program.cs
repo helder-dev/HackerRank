a9e8abbf551163d05797786b81ff8b005860fff1
helder-dev <helder.oliveira.dev@gmail.com>
2021-02-24 17:05:45 +0000

    Added solution to 'Viral Advertising' problem

---
+++ Problem Solving/Implementation/Viral Advertising/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Viral_Advertising {
+    class Program {
+        public static int ViralAdvertising(int n) {
+            int peopleSharing = 5,
+                cumulativeLikes = 2;
+
+            for (int i = 1; i < n; i++) {
+                peopleSharing = (peopleSharing / 2) * 3;
+                cumulativeLikes += (peopleSharing / 2);
+            }
+
+            return cumulativeLikes;
+        }
+
+        static void Main(string[] args) {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int result = ViralAdvertising(n);
+
+            Console.WriteLine(result);
+        }
+    }
+}
