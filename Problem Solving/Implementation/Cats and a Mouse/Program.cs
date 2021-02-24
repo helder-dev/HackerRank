e7ac4d481dc9c483115a00785fd5335389da18d0
helder-dev <helder.oliveira.dev@gmail.com>
2021-02-24 12:21:35 +0000

    Added solution to 'Cats and a Mouse' problem

---
+++ Problem Solving/Implementation/Cats and a Mouse/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Cats_and_a_Mouse {
+    class Program {
+        static string CatAndMouse(int x, int y, int z) {
+            if (Math.Abs(z - x) < Math.Abs(z - y)) {
+                return "Cat A";
+            } else if (Math.Abs(z - x) > Math.Abs(z - y)) {
+                return "Cat B";
+            } else {
+                return "Mouse C";
+            }
+        }
+
+        static void Main(string[] args) {
+            int q = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < q; i++) {
+                string[] xyz = Console.ReadLine().Split(' ');
+
+                int x = Convert.ToInt32(xyz[0]);
+                int y = Convert.ToInt32(xyz[1]);
+                int z = Convert.ToInt32(xyz[2]);
+
+                Console.WriteLine("{0}", CatAndMouse(x, y, z));
+            }
+        }
+    }
+}
