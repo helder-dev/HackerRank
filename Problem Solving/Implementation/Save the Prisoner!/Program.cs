09f3fdc893f9466a835f2bfc772cd8adbf654a48
helder-dev <helder.oliveira.dev@gmail.com>
2021-02-24 17:27:37 +0000

    Added solution to 'Save the Prisoner!' problem

---
+++ Problem Solving/Implementation/Save the Prisoner!/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Save_the_Prisoner_ {
+    class Program {
+        static int SaveThePrisoner(int n, int m, int s) {
+            return ((m - 1) + (s - 1)) % n + 1;
+        }
+
+        static void Main(string[] args) {
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < t; i++) {
+                string[] nms = Console.ReadLine().Split(' ');
+
+                int n = Convert.ToInt32(nms[0]);
+                int m = Convert.ToInt32(nms[1]);
+                int s = Convert.ToInt32(nms[2]);
+
+                int result = SaveThePrisoner(n, m, s);
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
