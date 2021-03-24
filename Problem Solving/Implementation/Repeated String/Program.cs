561e83f82006bbb5fc891c2beb1904d97653a97c
helder-dev <helder.oliveira.dev@gmail.com>
2021-03-24 15:27:19 +0000

    Added solution to 'Repeated String' problem

---
+++ Problem Solving/Implementation/Repeated String/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Repeated_String {
+    class Program {
+        static long RepeatedString(string s, long n) {
+            long numberOfLettersInSubString = s.Count(c => c.Equals('a'));
+            if (numberOfLettersInSubString == 0) return 0;
+
+            long numberOfCompletedSubStrings = (long)(n / s.Length);
+
+            long frequencyOfFirstLetterInTheSubString = numberOfLettersInSubString * numberOfCompletedSubStrings;
+
+            int remainder = (int)(n % s.Length);
+
+            for (int i = 0; i < remainder; i++) {
+                if (s[i].Equals('a')) frequencyOfFirstLetterInTheSubString++;
+            }
+
+            return frequencyOfFirstLetterInTheSubString;
+        }
+
+        static void Main(string[] args) {
+            string s = Console.ReadLine();
+
+            long n = Convert.ToInt64(Console.ReadLine());
+
+            long result = RepeatedString(s, n);
+
+            Console.WriteLine(result);
+        }
+    }
+}
