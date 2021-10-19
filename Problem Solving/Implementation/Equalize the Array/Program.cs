462c7b8e2506fc280a18aee5d7f387fd6b838a3a
helder-dev <helder.oliveira.dev@gmail.com>
2021-10-19 15:44:47 +0100

    Added solution to 'Equalize the Array' problem

---
+++ Problem Solving/Implementation/Equalize the Array/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Equalize_the_Array {
+    class Program {
+        public static int EqualizeArray(int[] ar) {
+            return ar.Length - ar.GroupBy(x => x).Select(x => x.Count()).OrderByDescending(x => x).First();
+        }
+
+        static void Main(string[] args) {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+
+            int result = EqualizeArray(ar);
+
+            Console.WriteLine(result);
+        }
+    }
+}
