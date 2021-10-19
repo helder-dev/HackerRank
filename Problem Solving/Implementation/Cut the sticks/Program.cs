c9a0d7c005c4bc390536fc13aff7fe1611999589
helder-dev <helder.oliveira.dev@gmail.com>
2021-10-19 15:26:48 +0100

    Added solution to 'Cut the sticks' problem

---
+++ Problem Solving/Implementation/Cut the sticks/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Cut_the_sticks {
+    class Program {
+        public static void CutTheSticks(int[] ar) {
+            while (ar.Count() > 0) {
+                Console.WriteLine(ar.Count());
+
+                int min = ar.Min();
+                ar = ar.Select(x => x - min).Where(x => x > 0).ToArray();
+            }
+        }
+
+        static void Main(string[] args) {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+
+            CutTheSticks(ar);
+        }
+    }
+}
