0cf602a7acb95803a95dc47a1d4dac6847fe17bb
helder-dev <helder.oliveira.dev@gmail.com>
2021-10-22 23:27:29 +0100

    Added solution to 'Encryption' problem

---
+++ Problem Solving/Implementation/Encryption/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace Encryption {
+    class Program {
+        public static void Encryption(string s) {
+            string trimmedString = Regex.Replace(s, @"\s+", "");
+
+            int columns = (int)Math.Ceiling(Math.Sqrt(trimmedString.Length));
+
+            for (int i = 0; i < columns; i++) {
+                var counter = 0;
+                while (i + (counter * columns) < trimmedString.Length) {
+                    Console.Write(trimmedString[i + (counter * columns)]);
+                    counter++;
+                }
+                Console.Write(' ');
+            }
+        }
+
+        static void Main(string[] args) {
+            string s = Console.ReadLine();
+
+            Encryption(s);
+        }
+    }
+}
