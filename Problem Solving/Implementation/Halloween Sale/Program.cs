4556719a4c91768ef4bb801d7a6cf2059d506987
helder-dev <helder.oliveira.dev@gmail.com>
2021-11-16 12:58:12 +0000

    Added solution to 'Halloween Sale' problem

---
+++ Problem Solving/Implementation/Halloween Sale/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Halloween_Sale {
+    class Program {
+        public static int HowManyGames(int p, int d, int m, int s) {
+            int numberOfGames = 0;
+            while (s > 0 && (s - p) >= 0) {
+                numberOfGames++;
+                s -= p;
+
+                if ((p - d) > m) {
+                    p -= d;
+                } else {
+                    p = m;
+                };
+            }
+
+            return numberOfGames;
+        }
+
+        static void Main(string[] args) {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int p = Convert.ToInt32(firstMultipleInput[0]);
+
+            int d = Convert.ToInt32(firstMultipleInput[1]);
+
+            int m = Convert.ToInt32(firstMultipleInput[2]);
+
+            int s = Convert.ToInt32(firstMultipleInput[3]);
+
+            Console.WriteLine(HowManyGames(p, d, m, s));
+        }
+    }
+}
