a6d7e02b450c88acc6133c5ff465151051f78dc3
helder-dev <helder.oliveira.dev@gmail.com>
2022-01-26 12:03:52 +0000

    Added solution to 'Lisa's Workbook' problem

---
+++ Problem Solving/Implementation/Lisa Workbook/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lisa_Workbook {
+    class Program {
+        public static int Workbook(int n, int k, int[] ar) {
+            int specialProblems = 0;
+            int pageNumber = 1;
+
+            for (int i = 0; i < ar.Length; i++) {
+                for (int j = 1; j <= ar[i]; j++) {
+                    if (pageNumber == j) {
+                        specialProblems++;
+                    }
+
+                    if ((j % k == 0) || (j == ar[i])) {
+                        pageNumber++;
+                    }
+                }
+            }
+
+            return specialProblems;
+        }
+
+        static void Main(string[] args) {
+            string[] nk = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nk[0]);
+            int k = Convert.ToInt32(nk[1]);
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+
+            int result = Workbook(n, k, ar);
+
+            Console.WriteLine(result);
+        }
+    }
+}
