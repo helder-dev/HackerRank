a95a885b92ef3339ab102df136c7f5f013bfcd56
helder-dev <helder.oliveira.dev@gmail.com>
2022-01-26 11:59:49 +0000

    Added solution to 'Service Lane' problem

---
+++ Problem Solving/Implementation/Service Lane/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Service_Lane {
+    class Program {
+        public static int[] ServiceLane(int[] width, int[][] cases) {
+            int[] result = new int[cases.Length];
+
+            for (int i = 0; i < cases.Length; i++) {
+                result[i] = width.Skip(cases[i][0]).Take((cases[i][1] - cases[i][0]) + 1).ToArray().Min();
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args) {
+            string[] nt = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nt[0]);
+            int t = Convert.ToInt32(nt[1]);
+
+            int[] width = Array.ConvertAll(Console.ReadLine().Split(' '), widthTemp => Convert.ToInt32(widthTemp));
+
+            int[][] cases = new int[t][];
+
+            for (int i = 0; i < t; i++) {
+                cases[i] = Array.ConvertAll(Console.ReadLine().Split(' '), casesTemp => Convert.ToInt32(casesTemp));
+            }
+
+            int[] result = ServiceLane(width, cases);
+
+            Console.WriteLine(string.Join("\n", result));
+        }
+    }
+}
