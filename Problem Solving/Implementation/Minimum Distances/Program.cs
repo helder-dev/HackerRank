b7855bf626e7cdc468a7a6c9ecb05dcd10fda874
helder-dev <helder.oliveira.dev@gmail.com>
2022-01-26 11:48:23 +0000

    Added solution to 'Minimum Distances' problem

---
+++ Problem Solving/Implementation/Minimum Distances/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Minimum_Distances {
+    class Program {
+        public static int MinimumDistances(int[] ar) {
+            int minDistance = -1;
+
+            for (int i = 0; i < ar.Length; i++) {
+                for (int j = i + 1; j < ar.Length; j++) {
+                    if (ar[i] == ar[j]) {
+                        if (minDistance == -1 || minDistance > (j - i)) {
+                            minDistance = (j - i);
+                        }
+                        break;
+                    }
+                }
+            }
+
+            return minDistance;
+        }
+
+        static void Main(string[] args) {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp));
+
+            int result = MinimumDistances(ar);
+
+            Console.WriteLine(result);
+        }
+    }
+}
