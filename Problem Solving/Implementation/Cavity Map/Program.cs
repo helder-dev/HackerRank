a410352147380265aff6b2fef990002223a6598d
helder-dev <helder.oliveira.dev@gmail.com>
2023-04-04 18:31:00 +0100

    Added solution to 'Cavity Map' problem

---
+++ Problem Solving/Implementation/Cavity Map/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Cavity_Map {
+    class Program {
+        public static char[, ] CavityMap(int length, char[, ] cavities) {
+            for (int row = 1; row < length - 1; row++) {
+                for (int col = 1; col < length - 1; col++) {
+                    if ((cavities[row - 1, col] < cavities[row, col]) &&
+                        (cavities[row, col + 1] < cavities[row, col]) &&
+                        (cavities[row + 1, col] < cavities[row, col]) &&
+                        (cavities[row, col - 1] < cavities[row, col])) {
+
+                        cavities[row, col] = 'X';
+                    } 
+                }
+            }
+
+            return cavities;
+        }
+
+        static void Main(string[] args) {
+            int length = Convert.ToInt32(Console.ReadLine().Trim());
+
+            char[, ] cavities = new char[length, length];
+            for (int i = 0; i < length; i++) {
+                char[] row = Console.ReadLine().ToCharArray();
+                for (int j = 0; j < length; j++) {
+                    cavities[i, j] = row[j];
+                }
+            }
+
+            char[, ] cavityMap = CavityMap(length, cavities);
+            for (int i = 0; i < length; i++) {
+                for (int j = 0; j < length; j++) {
+                    Console.Write(cavityMap[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
