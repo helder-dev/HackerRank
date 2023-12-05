c6f939f9886007f0d630709c35c07888a4fff96a
helder-dev <helder.oliveira.dev@gmail.com>
2023-12-05 13:02:17 +0000

    Added solution to 'Strange Counter' problem

---
+++ Problem Solving/Implementation/Strange Counter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Strange_Counter {
+    class Program {
+        public static long StrangeCounter(long t) {
+            long limit = 3;
+
+            while (t > limit) {
+                t = t - limit;
+                limit = limit * 2;
+            }
+
+            return limit + 1 - t;
+        }
+
+        static void Main(string[] args) {
+            long t = Convert.ToInt64(Console.ReadLine().Trim());
+
+            long result = StrangeCounter(t);
+            Console.WriteLine(result);
+        }
+    }
+}
